1e111dce0b2689c32243b97017ed41a585ae511b
Milosz <milosz.wegorski.2021@mumail.ie>
2026-02-05 12:13:42 +0000

    New measurement scheme which randomizes the measurement points according to some specified value

--- Signal_source/Measurement_schemes.py
+++ Signal_source/Measurement_schemes.py
@@ -16,26 +16,26 @@ class MeasurementScheme(BaseClass):
 
 class UniformMeasurement(MeasurementScheme):
 
-    def __init__(self, *, start_time, end_time, num_points):
-        self.start_time = start_time
-        self.end_time = end_time
+    def __init__(self, *, start_position, end_position, num_points):
+        self.start_position = start_position
+        self.end_position = end_position
         self.num_points = num_points
 
-        self.name = f"Uniform distribution from {start_time} to {end_time}\
+        self.name = f"Uniform distribution from {start_position} to {end_position}\
  with {num_points} points"
 
     def _get_points(self):
-        return np.array(np.linspace(self.start_time, self.end_time, self.num_points))
+        return np.array(np.linspace(self.start_position, self.end_position, self.num_points))
     
     def _get_name(self):
         return self.name
     
     def _get_param_value(self, param_name):
         match param_name:
-            case 'start_time':
-                return self.start_time
-            case 'end_time':
-                return self.end_time
+            case 'start_position':
+                return self.start_position
+            case 'end_position':
+                return self.end_position
             case 'num_points':
                 return self.num_points
             case _:
@@ -43,50 +43,50 @@ class UniformMeasurement(MeasurementScheme):
     
     def _set_param_value(self, param_name, param_value):
         match param_name:
-            case 'start_time':
-                self.start_time = param_value
-            case 'end_time':
-                self.end_time = param_value
+            case 'start_position':
+                self.start_position = param_value
+            case 'end_position':
+                self.end_position = param_value
             case 'num_points':
                 self.num_points = param_value
             case _:
                 raise ValueError('Unknown parameter name') 
             
     def _get_param_names(self):
-        return ['start_time', 'end_time', 'num_points']
+        return ['start_position', 'end_position', 'num_points']
 
 class RandomizedUniformMeasurement(MeasurementScheme):
 
-    def __init__(self, *, start_time, end_time, num_points, randomization):
-        self.start_time = start_time
-        self.end_time = end_time
+    def __init__(self, *, start_position, end_position, num_points, randomization):
+        self.start_position = start_position
+        self.end_position = end_position
         self.num_points = num_points
         self.randomization_percentage = randomization
 
-        self.name = f"Uniform distribution from {start_time} to {end_time}\
+        uniform_scheme = np.array(np.linspace(self.start_position, self.end_position, self.num_points))
+
+        step = uniform_scheme[1] - uniform_scheme [0]
+
+        self.randomized_scheme = np.array(uniform_scheme + 
+                            np.random.normal(0.0, step *self.randomization_percentage,
+                                             size=uniform_scheme.shape))
+
+        self.name = f"Uniform distribution from {start_position} to {end_position}\
  with {num_points} points"
 
     def _get_points(self):
 
-        uniform_scheme = np.array(np.linspace(self.start_time, self.end_time, self.num_points))
-
-        step = uniform_scheme[1] - uniform_scheme [0]
-
-        randomized_scheme = np.array(uniform_scheme + 
-                            np.random.normal(0.0, step *self.randomization_percentage,
-                                             size= uniform_scheme.shape))
-
-        return randomized_scheme
+        return self.randomized_scheme
     
     def _get_name(self):
         return self.name
     
     def _get_param_value(self, param_name):
         match param_name:
-            case 'start_time':
-                return self.start_time
-            case 'end_time':
-                return self.end_time
+            case 'start_position':
+                return self.start_position
+            case 'end_position':
+                return self.end_position
             case 'num_points':
                 return self.num_points
             case _:
@@ -94,26 +94,59 @@ class RandomizedUniformMeasurement(MeasurementScheme):
     
     def _set_param_value(self, param_name, param_value):
         match param_name:
-            case 'start_time':
-                self.start_time = param_value
-            case 'end_time':
-                self.end_time = param_value
+            case 'start_position':
+                self.start_position = param_value
+            case 'end_position':
+                self.end_position = param_value
             case 'num_points':
                 self.num_points = param_value
             case _:
                 raise ValueError('Unknown parameter name') 
             
     def _get_param_names(self):
-        return ['start_time', 'end_time', 'num_points']
+        return ['start_position', 'end_position', 'num_points']
+    
+
+class CustomArrayMeasurement(MeasurementScheme):
+
+    def __init__(self, *, point_array):
+        self.point_array = point_array
+
+        self.name = f"Returns custom array as measurement scheme"
+
+    def _get_points(self):
+
+        return np.array(self.point_array)
+    
+    def _get_name(self):
+        return self.name
+    
+    def _get_param_value(self, param_name):
+        match param_name:
+            case 'point_array':
+                return self.point_array
+            case _:
+                raise ValueError(f'Unknown parameter name {param_name}') 
+    
+    def _set_param_value(self, param_name, param_value):
+        match param_name:
+            case 'point_array':
+                return self.point_array == param_value
+            case _:
+                raise ValueError('Unknown parameter name') 
+            
+    def _get_param_names(self):
+        return ['point_array']
+
 
 class LogMeasurement(MeasurementScheme):
 
-    def __init__(self, start_time, end_time, num_points):
-        self.start_time = start_time
-        self.end_time = end_time
+    def __init__(self, start_position, end_position, num_points):
+        self.start_position = start_position
+        self.end_position = end_position
         self.num_points = num_points
 
-        self.name = f"Logarithmic distribution from {start_time} to {end_time}\
+        self.name = f"Logarithmic distribution from {start_position} to {end_position}\
  with {num_points} points"
 
     def _get_points(self):
@@ -124,10 +157,10 @@ class LogMeasurement(MeasurementScheme):
     
     def _get_param_value(self, param_name):
         match param_name:
-            case 'start_time':
-                return self.start_time
-            case 'end_time':
-                return self.end_time
+            case 'start_position':
+                return self.start_position
+            case 'end_position':
+                return self.end_position
             case 'num_points':
                 return self.num_points
             case _:
@@ -135,17 +168,17 @@ class LogMeasurement(MeasurementScheme):
     
     def _set_param_value(self, param_name, param_value):
         match param_name:
-            case 'start_time':
-                self.start_time = param_value
-            case 'end_time':
-                self.end_time = param_value
+            case 'start_position':
+                self.start_position = param_value
+            case 'end_position':
+                self.end_position = param_value
             case 'num_points':
                 self.num_points = param_value
             case _:
                 raise ValueError(f'Unknown parameter name {param_name}') 
             
     def _get_param_names(self):
-        return ['start_time', 'end_time', 'num_points']
+        return ['start_position', 'end_position', 'num_points']
     
 
 class DummyMeasurementScheme(MeasurementScheme):
